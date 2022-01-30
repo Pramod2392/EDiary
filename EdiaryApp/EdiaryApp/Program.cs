5d875db195b8b49efdf15a4753b991918bfb94b5
Pramod S <pramod2392@gmail.com>
2022-01-30 15:46:19 +0530

    The folder where the EDairy file is saved is made configurable. Feature: Get Summary for a given day is added.

--- EdiaryApp/EdiaryApp/Program.cs
+++ EdiaryApp/EdiaryApp/Program.cs
@@ -1,59 +1,89 @@
-﻿Console.WriteLine("Hi Pramod!!");
-Console.WriteLine(value: questionConstants.WakeUpQuestion);
-Console.WriteLine("Format hh:mm");
-string? wakeUpTime = Console.ReadLine();
-if (wakeUpTime != null)
+﻿using System.Configuration;
+
+Console.WriteLine("Hi Pramod!!");
+Console.WriteLine("Do you wish to get the details of a partiular day? If Yes, press 1, otherwise press 0");
+if(int.TryParse(Console.ReadLine(),out int value))
 {
-    try
+    if(value == 1)
     {
-        int hour = Convert.ToInt32(wakeUpTime.Split(":")[0]);
-        int minute = Convert.ToInt32(wakeUpTime.Split(':')[1]);
-        DateTime wakeUpDateTime = new
-            DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day,
-                    hour, minute, DateTime.Now.Second);
+        Console.WriteLine("Enter the date for which you need the summary in dd:mm:yyyy format");
+        string? dateTimeStr = Console.ReadLine();
+        if (!string.IsNullOrEmpty(dateTimeStr))
+        {
+            string[] splitDate = dateTimeStr.Split(new char[] { ':' });
+            DateOnly date = new DateOnly(Convert.ToInt32(splitDate[2]), Convert.ToInt32(splitDate[1]), Convert.ToInt32(splitDate[0]));
+            string? rootDirectory = ConfigurationManager.AppSettings.Get("EdairyFolder");
+            FileInfo FileInfo = new FileInfo($"{rootDirectory}{DateTime.Now.ToShortDateString()}");
+
+            using (StreamReader reader = new StreamReader(FileInfo.FullName))
+            {
+                Console.WriteLine(reader.ReadToEnd()); 
+            }
+        }
     }
-    catch (FormatException exF)
+
+    else if(value == 0)
     {
-        Console.WriteLine("Input Hour and minute is not in expected format");
-        Console.WriteLine(exF.Message);
-        Console.WriteLine("Re-launch the application and give the correct format");
+        Console.WriteLine(value: questionConstants.WakeUpQuestion);
+        Console.WriteLine("Format hh:mm");
+        string? wakeUpTime = Console.ReadLine();
+        if (wakeUpTime != null)
+        {
+            try
+            {
+                int hour = Convert.ToInt32(wakeUpTime.Split(":")[0]);
+                int minute = Convert.ToInt32(wakeUpTime.Split(':')[1]);
+                DateTime wakeUpDateTime = new
+                    DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day,
+                            hour, minute, DateTime.Now.Second);
+            }
+            catch (FormatException exF)
+            {
+                Console.WriteLine("Input Hour and minute is not in expected format");
+                Console.WriteLine(exF.Message);
+                Console.WriteLine("Re-launch the application and give the correct format");
+            }
+
+        }
+        Console.WriteLine(questionConstants.todayPlanQuestion);
+        string? todayPlans = Console.ReadLine();
+
+        Console.WriteLine(questionConstants.howAreYouQuestion);
+        string? howAreYou = Console.ReadLine();
+
+        Console.WriteLine(questionConstants.positiveQuestion);
+        string? positives = Console.ReadLine();
+
+        Console.WriteLine(questionConstants.negativeQuestion);
+
+        string? negatives = Console.ReadLine();
+
+        string? rootDirectory =  ConfigurationManager.AppSettings.Get("EdairyFolder");
+
+        FileInfo FileInfo = new FileInfo($"{rootDirectory}{DateTime.Now.ToShortDateString()}");
+
+        using (StreamWriter sw = new StreamWriter(FileInfo.FullName))
+        {
+            sw.WriteLine(questionConstants.WakeUpQuestion);
+
+            sw.WriteLine($" {wakeUpTime}");
+
+            sw.WriteLine(questionConstants.todayPlanQuestion);
+
+            sw.WriteLine($" {todayPlans}");
+
+            sw.WriteLine(questionConstants.howAreYouQuestion);
+
+            sw.WriteLine($" {howAreYou}");
+
+            sw.WriteLine(questionConstants.positiveQuestion);
+
+            sw.WriteLine($" {positives}");
+
+            sw.WriteLine(questionConstants.negativeQuestion);
+
+            sw.WriteLine($" {negatives}");
+        }
+
     }
-
-}
-Console.WriteLine(questionConstants.todayPlanQuestion);
-string? todayPlans = Console.ReadLine();
-
-Console.WriteLine(questionConstants.howAreYouQuestion);
-string? howAreYou = Console.ReadLine();
-    
-Console.WriteLine(questionConstants.positiveQuestion);
-string? positives = Console.ReadLine();
-
-Console.WriteLine(questionConstants.negativeQuestion);
-
-string? negatives = Console.ReadLine();
-
-FileInfo FileInfo = new FileInfo($"C:\\Users\\Pramod\\Desktop\\EDairy\\{DateTime.Now.ToShortDateString()}");
-
-using (StreamWriter sw = new StreamWriter(FileInfo.FullName))
-{
-    sw.WriteLine(questionConstants.WakeUpQuestion);
-    
-    sw.WriteLine($" {wakeUpTime}");
-    
-    sw.WriteLine(questionConstants.todayPlanQuestion);
-    
-    sw.WriteLine($" {todayPlans}");
-
-    sw.WriteLine(questionConstants.howAreYouQuestion);
-
-    sw.WriteLine($" {howAreYou}");
-
-    sw.WriteLine(questionConstants.positiveQuestion);
-    
-    sw.WriteLine($" {positives}");
-    
-    sw.WriteLine(questionConstants.negativeQuestion);
-    
-    sw.WriteLine($" {negatives}");
 }
